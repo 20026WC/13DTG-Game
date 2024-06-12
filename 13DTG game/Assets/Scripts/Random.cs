c78c13735a4c312ef1e9e030748207886f5048f5
20026WC <20026@wc.school.nz>
2024-06-12 12:25:25 +1200

    Testing for setting objects active to true and false
    Testing for making objects active true and false

---
+++ 13DTG game/Assets/Scripts/Random.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Random : MonoBehaviour
+{
+    public GameObject Plane;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Plane.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
