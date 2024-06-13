a2085e7759c6ae71302d7b0bf7b8e8f2392663db
20026WC <20026@wc.school.nz>
2024-06-13 15:11:57 +1200

    Random no generator
    Does not work

--- 13DTG game/Assets/Scripts/Random.cs
+++ 13DTG game/Assets/Scripts/Random.cs
@@ -5,15 +5,32 @@ using UnityEngine;
 public class Random : MonoBehaviour
 {
     public GameObject Plane;
+    public GameObject Piller;
+    public GameObject Piller2;
+
     // Start is called before the first frame update
     void Start()
     {
-        Plane.SetActive(false);
+        
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        System.Random ran = new System.Random();
+        int randomInt = ran.Next(1, 3);
+
+        if ( ran = 1)
+        {
+            Plane.SetActive(false);
+        }
+        else if (ran = 2)
+        {
+            Piller.SetActive(false);
+        }
+        else
+        {
+            Piller2.SetActive(false);
+        }
     }
 }
